3ddae738f6529867984e4ef06c793328e21ebb3b
KizzyCode <development@kizzycode.de>
2020-05-16 01:50:05 +0200

    Added an iterator for DERSequence

--- Sources/Asn1Der/Typed/Sequence.swift
+++ Sources/Asn1Der/Typed/Sequence.swift
@@ -1,6 +1,39 @@
 import Foundation
 
 
+/// An iterator over a DER sequence object
+final public class DERSequenceIterator {
+	private let sequence: DERSequence
+	private var position = 0
+	
+	/// Initializes the iterator with a `sequence`
+	init(sequence: DERSequence) {
+		self.sequence = sequence
+	}
+	
+	/// Returns the next element as `T`
+	public func next<T: DERObject>() throws -> T {
+		guard let next = self.next() else {
+			throw DERError.invalidData("Not enough elements in sequence")
+		}
+		return try T(with: next.object())
+	}
+}
+extension DERSequenceIterator: IteratorProtocol {
+	public typealias Element = DERObject
+	
+	public func next() -> DERObject? {
+		switch self.sequence.value.count {
+			case let count where self.position < count:
+				defer{ self.position += 1 }
+				return self.sequence.value[self.position]
+			default:
+				return nil
+		}
+	}
+}
+
+
 /// A DER sequence object
 final public class DERSequence: DERObject {
 	/// The associated DER tag
